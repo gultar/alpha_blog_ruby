a197189e4481ad9ce12eaf43c5215c83c5a9df01
gultar <sacha-olivier.dulac@hotmail.com>
2022-10-18 19:21:25 -0500

    had to start new repo, the other one was stuck

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController 
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        #render inline: params[:article].inspect
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            # if(@article.errors.any?)
+            #     @article.errors.full_messages.each { |msg| flash[msg] = msg }
+                
+            # end
+            render :new
+        end
+
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+end
